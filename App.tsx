a57c66d520eabd16ad8bd23f09e95785a8117c12
saziye <szy.dag2222@gmail.com>
2026-01-18 18:48:44 +0300

    Add App.tsx

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from './src/navigation/AppNavigator';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+        <NavigationContainer>
+            <AppNavigator />
+            <StatusBar style="light" />
+        </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
